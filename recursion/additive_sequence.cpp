9f7d8dd31510963edfe27e693e382c908e83c03e
suraj-singh12 <surajsingh5092@gmail.com>
2022-09-02 11:05:47 +0530

    additive sequence check

--- recursion/additive_sequence.cpp
+++ recursion/additive_sequence.cpp
@@ -1,3 +1,91 @@
 // given a string, check that if it is additive sequence or not
 // 47111829 (4 7 11 18 29) yes
 // 413173047 (413 17 30 47) yes
+
+#include <iostream>
+#include <string> 
+using namespace std;
+
+string addStrings(string a, string b) {
+    string result = "";
+
+    int carry = 0;
+    int i = a.length() - 1;
+    int j = b.length() - 1;
+
+    while (i >= 0 && j >= 0) {
+        int sum = 0;
+        sum += a[i] - '0';
+        sum += b[j] - '0';
+        sum += carry;
+        carry = sum / 10;
+        result = to_string(sum % 10) + result;
+        i--; j--;
+    }
+
+    while(i >= 0) {
+        int sum = 0;
+        sum += a[i] - '0';
+        sum += carry;
+        carry = sum / 10;
+        result = to_string(sum % 10) + result;
+        i--;
+    }
+
+    while(j >= 0) {
+        int sum = 0;
+        sum += b[j] - '0';
+        sum += carry;
+        carry = sum / 10;
+        result = to_string(sum % 10) + result;
+        j--;
+    }
+
+    if(carry > 0) {
+        result = to_string(carry) + result;
+    }
+    return result;
+}
+
+bool check(string a, string b, string c) {
+    // check if the strings are valid or not 
+    if((a.length() > 1 && a[0] == '0') || (b.length() > 1 && b[0] == '0')) {
+        return false;
+    }
+    // add a and b, and check if == c
+    string res = addStrings(a, b);
+    if(res == c) {
+        return true;
+    }
+
+    // if the res (sum) length is greater or equal to remaining string (c), and they are not matched (acc to above condition),
+    // or if result is not a substring of c,
+    // then no additive sequence possible in this pattern further, 
+    if(res.length() >= c.length() || res != c.substr(0, res.length())) {
+        return false;
+    } else {
+        // otherwise, do check for remaining string (c)
+        return check(b, res, c.substr(res.length()));
+    }
+}
+
+string isAdditiveSequence(string str) {
+    // addition result cannot be smaller than any of the operands, so logically we need to go till length/2
+    for(int i = 1; i < str.length() / 2; ++i) {
+        for(int j = 1; j < (str.length() - i) / 2; ++j) {
+            bool isAdditive = check(str.substr(0,i), str.substr(i,j), str.substr(i+j));
+            if(isAdditive) {
+                return "YES";
+            }
+        }
+    }
+    return "NO";
+}
+
+int main()
+{
+    string str = "235813";
+    str = "213242751";
+    cout << isAdditiveSequence(str) << endl;
+    return 0;
+}
---
+++ recursion/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str = "abcdefghi";
+    cout << str.substr(0,0) << endl;
+    cout << str.substr(1,1) << endl;
+    cout << str.substr(2,2) << endl;
+    cout << str.substr(2) << endl;
+    cout << to_string(534) << endl;
+    return 0;
+}
+
+// 10
+// 1-5  1-5
+// 2, 1-5 1-4
+// 3, 1-5 1-3
+// 4, 1-5 1-2
+// 5, 1-5 1-1
